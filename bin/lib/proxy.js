c5ebb20d9ecad72782d567cbac63b319da681327
pinkipi <yarnovax@gmail.com>
2018-11-02 20:32:18 -0400

    Blacklist Node.JS 11.1.0

--- bin/lib/proxy.js
+++ bin/lib/proxy.js
@@ -1,6 +1,6 @@
-if(process.versions.node === '11.0.0') {
-	console.error(`Error: Node.JS 11.0.0 contains a critical bug preventing timers from working.
-Please install a newer version (if available) or revert to 10.12.0.`)
+if(['11.0.0', '11.1.0'].includes(process.versions.node)) {
+	console.error(`Error: Node.JS ${process.versions.node} contains a critical bug preventing timers from working.
+Please install a newer version (if available) or revert to 10.13.0.`)
 	process.exit()
 }
 

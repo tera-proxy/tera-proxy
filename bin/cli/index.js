8a93c20e93c5ec36c014c5c05e784ed6a2c8b30f
pinkipi <yarnovax@gmail.com>
2019-07-24 12:24:02 -0400

    Switch to WinDivert

--- bin/cli/index.js
+++ bin/cli/index.js
@@ -4,21 +4,36 @@ const logRoot = require('log'),
 if(['11.0.0', '11.1.0', '11.2.0', '11.3.0'].includes(process.versions.node)) {
 	log.error(`Node.JS ${process.versions.node} contains a critical bug preventing timers from working.
 Please install a newer version or revert to 10.14.1 LTS.`)
-	process.exit()
+	return
 }
 if(typeof BigInt === 'undefined' || require('module').createRequireFromPath === undefined) {
 	log.error(`Your version of Node.JS is outdated.
 Please install the latest Current from https://nodejs.org/`)
-	process.exit()
+	return
+}
+if(process.platform !== 'win32') {
+	log.error('TERA Proxy only supports Windows.')
+	return
 }
 
-async function init() {
-	const path = require('path'),
-		settings = require('../../settings/_tera-proxy_.json')
+const net = require('net'),
+	path = require('path'),
+	settings = require('../../settings/_tera-proxy_.json')
 
-	log.info(`Node version: ${process.versions.node}, game region: ${settings.region}`)
+;(async () => {
+	log.info(`Node version: ${process.versions.node}`)
 
-	if(settings.devWarnings) require('log').level = 'dwarn'
+	try {
+		await new Promise((resolve, reject) => {
+			net.createServer().listen('\\\\.\\pipe\\tera-proxy', resolve).on('error', reject)
+		})
+	}
+	catch(e) {
+		log.error('Another instance of TERA Proxy is already running. Please close it then try again.')
+		return
+	}
+
+	if(settings.devWarnings) logRoot.level = 'dwarn'
 
 	if(settings.autoUpdate) {
 		log.info('Checking for updates')
@@ -43,59 +58,12 @@ async function init() {
 		}
 	}
 
-	const SlsProxy = require('tera-proxy-sls'),
+	const ProxyGame = require('proxy-game'),
 		{ ModManager, Dispatch, Connection, RealClient } = require('tera-proxy-game'),
-		{ protocol } = require('tera-data-parser'),
-		regions = require('./regions'),
-		currentRegion = regions[settings.region]
+		servers = require('./servers')
 
-	if(!currentRegion) {
-		log.error('Unsupported region: ' + settings.region)
-		return
-	}
+	let initialized = false
 
-	const fs = require('fs'),
-		net = require('net'),
-		dns = require('dns'),
-		url = require('url'),
-		hosts = require('./hosts')
-
-	const sls = new SlsProxy(currentRegion),
-		slsProxyIp = '127.0.0.' + (10 + currentRegion.index)
-
-	// Test if we're allowed to modify the hosts file
-	try { hosts.remove() }
-	catch(e) {
-		switch(e.code) {
-			case 'EBUSY':
-				log.error(`Hosts file is being locked by another application.
-
-* Completely uninstall any anti-virus software.`)
-				break
-			case 'EACCES':
-				log.error(`Hosts file is set to read-only.
-
-* Make sure no anti-virus software is running.
-* Locate "${e.path}", right click the file, click 'Properties', uncheck 'Read-only' then click 'OK'.`)
-				break
-			case 'EPERM':
-				log.error(`Insufficient permission to modify hosts file.
-
-* Make sure no anti-virus software is running.
-* Right click TeraProxy.bat and select 'Run as administrator'.`)
-				break
-			default:
-				throw e
-		}
-
-		process.exit(1)
-	}
-
-	const servers = new Map()
-
-	dns.setServers(['8.8.8.8', '8.8.4.4'])
-
-	// Init
 	const modManager = new ModManager({
 		modsDir: path.join(__dirname, '..', '..', 'mods'),
 		settingsDir: path.join(__dirname, '..', '..', 'settings'),
@@ -104,161 +72,119 @@ async function init() {
 
 	await modManager.init()
 
-	// Retrieve server list
-	let serverList
+	const redirects = [],
+		serverQ = []
+
+	for(let data of servers) {
+		let redirect
+
+		const server = net.createServer(socket => {
+			if(!initialized) { // Should never happen, but would result in an infinite loop otherwise
+				socket.end()
+				return
+			}
+
+			const logThis = log(`client ${socket.remoteAddress}:${socket.remotePort}`)
+
+			socket.setNoDelay(true)
+
+			const dispatch = new Dispatch(modManager),
+				connection = new Connection(dispatch, { classic: data.type === 'classic' }),
+				client = new RealClient(connection, socket),
+				srvConn = connection.connect(client, { host: redirect[2], port: redirect[3] }) // Connect to self to bypass redirection
+
+			logThis.log('connecting')
+
+			dispatch.once('init', () => {
+				dispatch.region = data.region
+				dispatch.loadAll()
+			})
+
+			socket.on('error', err => {
+				if(err.code === 'ECONNRESET') logThis.log('lost connection to client')
+				else logThis.warn(err)
+			})
+
+			srvConn.on('connect', () => { logThis.log(`connected to ${srvConn.remoteAddress}:${srvConn.remotePort}`) })
+
+			srvConn.on('error', err => {
+				if(err.code === 'ECONNRESET') logThis.log('lost connection to server')
+				else if(err.code === 'ETIMEDOUT') logThis.log('timed out waiting for server response')
+				else logThis.warn(err)
+			})
+
+			srvConn.on('close', () => { logThis.log('disconnected') })
+		})
+
+		serverQ.push(new Promise((resolve, reject) => {
+			server.listen(0, '127.0.0.2', resolve).on('error', reject)
+		}).then(() => {
+			const addr = server.address()
+			redirects.push(redirect = [data.ip, data.port, addr.address, addr.port])
+		}))
+	}
+
+	await Promise.all(serverQ)
+
 	try {
-		serverList = await sls.fetch()
+		// Swap gameserver addresses with proxy ones
+		const pg = new ProxyGame(`tcp && (${
+			['127.0.0.2', ...new Set(servers.map(s => s.ip))].map(ip => `ip.SrcAddr == ${ip}||ip.DstAddr==${ip}`).join('||')
+		})`, ...redirects)
+
+		setInterval(() => { pg }, 60000) // TODO: Store object in C++ memory and only delete on close()
 	}
 	catch(e) {
+		let msg = null
+
 		switch(e.code) {
-			case 'ETIMEDOUT':
-				log.error(`Server list timed out. Please check your internet connection and try again.`)
+			case 2:
+				msg = [
+					'Failed to load WinDivert driver file.',
+					'Start TERA Proxy prior to any VPN software.',
+					'Make sure anti-virus software did not delete required files.',
+					'Open an administrator command prompt and enter \'sc stop windivert1.4\'.'
+				]
+				break
+			case 5:
+				msg = [
+					'Access denied.',
+					'Right click TeraProxy.bat and select \'Run as administrator\'.',
+					'Disable or uninstall your anti-virus software.'
+				]
+				break
+			case 577:
+				msg = [
+					'WinDivert driver signature could not be verified.',
+					'Update Windows.',
+					'If using Windows 7 or earlier, upgrade to a later version.'
+				]
+				break
+			case 1275:
+				msg = [
+					'WinDivert driver was blocked.',
+					'Uninstall your anti-virus software completely, then restart your computer.'
+				]
+				break
+			case 1753:
+				msg = [
+					'Base Filtering Engine service is disabled.',
+					'Run "services.msc".',
+					'Right-click the "Base Filtering Engine" service, select \'Properties\'.',
+					'Change \'Startup type\' to \'Automatic\', then click \'Start\'.'
+				]
 				break
 			default:
 				throw e
 		}
 
+		log.error(`${msg.shift()}${msg.length > 1 ? '\n' : ''}${msg.map(s => '\n* ' + s).join('')}`)
 		process.exit(1)
 	}
 
-	// Create game proxies for all servers
-	const customServers = sls.customServers = { tag: currentRegion.tag }
-	{
-		let i = 0
-		for(let id in serverList) {
-			const target = serverList[id]
-
-			customServers[id] = {
-				ip: '127.0.0.' + (20 + i++),
-				port: 9250 + currentRegion.index,
-				keepOriginal: true
-			}
-
-			const server = net.createServer(socket => {
-				const logThis = log(`client ${socket.remoteAddress}:${socket.remotePort}`)
-
-				socket.setNoDelay(true)
-
-				const dispatch = new Dispatch(modManager),
-					connection = new Connection(dispatch, { classic: settings.region.split('-')[1] === 'CLASSIC' }),
-					client = new RealClient(connection, socket),
-					srvConn = connection.connect(client, { host: target.ip, port: target.port })
-
-				logThis.log('connecting')
-
-				connection.dispatch.once('init', () => {
-					connection.dispatch.region = settings.region.split('-')[0].toLowerCase()
-
-					dispatch.loadAll()
-				})
-
-				socket.on('error', err => {
-					if(err.code === 'ECONNRESET') logThis.log('lost connection to client')
-					else logThis.warn(err)
-				})
-
-				srvConn.on('connect', () => {
-					logThis.log(`connected to ${srvConn.remoteAddress}:${srvConn.remotePort}`)
-				})
-
-				srvConn.on('error', err => {
-					if(err.code === 'ECONNRESET') logThis.log('lost connection to server')
-					else logThis.warn(err)
-				})
-
-				srvConn.on('close', () => { logThis.log('disconnected') })
-			})
-
-			servers.set(id, server)
-		}
-	}
-
-	// Run SLS proxy
-	try {
-		await sls.listen(slsProxyIp)
-	}
-	catch(e) {
-		if(e.code === 'EADDRINUSE') {
-			log.error('Another instance of TeraProxy is already running, please close it then try again.')
-			process.exit()
-		}
-		if(e.code === 'EACCES') {
-			log.error(`Another process is already using port ${sls.port}.\nPlease close or uninstall the application first:\n${require('./netstat')(sls.port)}`)
-			process.exit()
-		}
-		if(e.code === 'EADDRNOTAVAIL') {
-			log.error(`Loopback address not available. Restart your computer and try again.
-
-If this does not work:
-* Disable your firewall.
-* Disable or uninstall any third-party firewall or anti-virus software.`)
-			process.exit()
-		}
-		throw e
-	}
-	log.info(`${settings.region}-SLS listening on ${slsProxyIp}:${sls.port}`)
-
-	// Run game proxies
-	const gameProxyQ = []
-
-	for(let [id, server] of servers)
-		gameProxyQ.push(new Promise((resolve, reject) => {
-			server.listen(customServers[id].port, customServers[id].ip, resolve)
-			.on('error', reject)
-		}).then(() => {
-			const address = server.address()
-			log.info(`${settings.region}-${id} listening on ${address.address}:${address.port}`)
-		}))
-
-	try {
-		await Promise.all(gameProxyQ)
-	}
-	catch(e) {
-		if(e.code === 'EACCES') {
-			let port = customServers[0].port
-			log.error(`Another process is already using port ${port}.\nPlease close or uninstall the application first:\n${require('./netstat')(port)}`)
-			return require('./netstat')(port)
-		}
-		throw e
-	}
-
-	hosts.set(slsProxyIp, sls.host)
-	log.info('Added hosts file entry')
-
-	if(sls.https) {
-		sls.addRootCertificate()
-		log.info('Added SLS root certificate')
-	}
-
 	log.info('OK')
-
-	// Exit/crash handlers
-
-	function cleanup() {
-		try { hosts.remove(sls.host) } catch(e) {}
-
-		if(sls.https)
-			try { sls.delRootCertificate() } catch(e) {}
-
-		sls.close()
-		for(let server of servers.values()) server.close()
-	}
-
-	function cleanExit() {
-		log.info('Terminating...')
-		cleanup()
-		process.exit()
-	}
-
-	process.on('SIGHUP', cleanExit)
-	process.on('SIGINT', cleanExit)
-	process.on('SIGTERM', cleanExit)
-
-	process.on('uncaughtException', e => {
-		logRoot.error(e)
-		cleanup()
-		process.exitCode = 1
-	})
-}
-
-init()
+	initialized = true
+})().catch(e => {
+	log.error(e)
+	process.exit(1)
+})
--- bin/configurator.js
+++ bin/configurator.js
@@ -5,41 +5,12 @@ const fs = require('fs'),
 	readline = require('readline')
 
 const configDir = path.join(__dirname, '../settings'),
-	configFile = path.join(configDir, '_tera-proxy_.json'),
-	regions = [{
-		name: ['EU'],
-		publisher: 'Gameforge'
-	}, {
-		name: ['EU-TEST'],
-		publisher: 'Gameforge - Beta'
-	}, {
-		name: ['RU'],
-		publisher: 'Destiny.Games'
-	}, {
-		name: ['TW'],
-		publisher: 'HappyTuk / MangoT5'
-	}, {
-		name: ['JP'],
-		publisher: 'GameOn / Pmang'
-	}, {
-		name: ['TH', 'SE', 'ID'],
-		publisher: 'PlayWith / Lytogame'
-	}, {
-		name: ['NA'],
-		publisher: 'En Masse'
-	}, {
-		name: ['KR'],
-		publisher: 'Nexon'
-	}, {
-		name: ['KR-TEST'],
-		publisher: 'Nexon - Beta'
-	}]
+	configFile = path.join(configDir, '_tera-proxy_.json')
 
 let config = {
 	branch: 'master',
 	autoUpdate: true,
 	autoUpdateMods: true,
-	region: '',
 	devWarnings: false
 }
 
@@ -54,15 +25,6 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 function question(q) { return new Promise(resolve => { rl.question(q, resolve) }) }
 
 (async () => {
-	console.log(`Supported game regions:\n${regions.map(r => {
-		let str = `    ${r.name.join(' / ')}`
-		str += ' '.repeat(Math.max(0, 20 - str.length))
-		return str + (r.publisher ? `(${r.publisher})` : '')
-	}).join('\n')}\n`)
-
-	while(!setRegion(await question(`Game region${config.region ? ` (current = ${config.region})` : ''}: `)))
-		console.log('Invalid region')
-
 	config.autoUpdate = config.autoUpdateMods = parseBool(await question(`Automatically update proxy? (${config.autoUpdate ? 'Y/n' : 'y/N'}): `),
 		config.autoUpdate)
 
@@ -73,18 +35,6 @@ function question(q) { return new Promise(resolve => { rl.question(q, resolve) }
 	fs.writeFileSync(configFile, JSON.stringify(config, null, '\t'))
 	rl.close()
 })()
-
-function setRegion(region) {
-	if(!region) region = config.region
-
-	region = region.toUpperCase()
-	for(let r of regions)
-		if(r.name.includes(region)) {
-			config.region = r.name[0]
-			return true
-		}
-	return false
-}
 
 function parseBool(str, def) {
 	if(!str) return def
